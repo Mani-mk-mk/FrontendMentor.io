38737791f6656c5f4dc5b8d581360a5530999934
Manigandan Kasimani <72310576+Mani-mk-mk@users.noreply.github.com>
2022-10-26 19:08:11 +0530

    Update script.js

--- expenses-chart-component-main/scripts/script.js
+++ expenses-chart-component-main/scripts/script.js
@@ -4,7 +4,7 @@ const getJSON = async (path, callback) => {
 
 console.log("Hello");
 
-getJSON("../data.json", (response) => {
+getJSON("../expenses-chart-component-main/data.json", (response) => {
 	fillData(response);
 });
 
